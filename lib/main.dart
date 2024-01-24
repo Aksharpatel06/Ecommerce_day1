d12190450045399f2c55eef746023aafb4e961fe
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-01-24 18:58:09 +0530

    24-01-2024

--- lib/main.dart
+++ lib/main.dart
@@ -13,41 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          backgroundColor: Color(0xff7CB342),
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text('My app',style: TextStyle(
-              color: Colors.white,
-            ),),
-            backgroundColor: Color(0xff8BC34A),
-          ),
-          body: Center(
-            child: Container(
-              height: 400,
-              width: 400,
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                color: Color(0xff4CAF50),
-              ),
-              child: Container(
-                height: 300,
-                width: 300,
-                alignment: Alignment.center,
-                decoration:const BoxDecoration(
-                  color: Color(0xffB2FF59),
-                ),
-                child:  const Text('OOOO',style: TextStyle(
-                  letterSpacing: -50,
-                  fontSize: 150,
-                  color: Color(0xff597F2C),
-                ),),
-              ),
-            ),
-          ),
-        ),
-      ),
+
     );
   }
 }
