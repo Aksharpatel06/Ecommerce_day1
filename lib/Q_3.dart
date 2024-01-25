1641d229ab6e94930ca35ac11ad8f1b30e5908c7
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-01-25 16:27:05 +0530

    25-01-2024

--- lib/Q_3.dart
+++ lib/Q_3.dart
@@ -27,33 +27,46 @@ class MyApp extends StatelessWidget {
                 height: 400,
                 width: 400,
                 alignment: Alignment.center,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   border: Border(left: BorderSide(color:Color(0xff2196F3),width: 75 ),top:BorderSide(color:Color(0xff2196F3),width: 50)),
                 ),
               child: Container(
                 alignment: Alignment.center,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   border: Border(left: BorderSide(color:Color(0xffFFFF00),width: 50),top:BorderSide(color:Color(0xffFFFF00),width: 50)),
                 ),
                 child: Container(
                   height: 400,
                   width: 400,
                   alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide(color:Color(0xffE91E63),width: 50),right:BorderSide(color:Color(0xffE91E63),width: 50)),
+                  decoration: const BoxDecoration(
+                    border: Border(bottom: BorderSide(color:Color(0xffE91E63),width: 45),right:BorderSide(color:Color(0xffE91E63),width: 45)),
                   ),
                   child: Container(
-                  height: 400,
-                  width: 400,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                  border: Border(bottom: BorderSide(color:Color(0xffE91E63),width: 50),right:BorderSide(color:Color(0xffE91E63),width: 50)),
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                    border: Border(bottom: BorderSide(color:Color(0xffFF9800),width: 35),right:BorderSide(color:Color(0xffFF9800),width: 35)),
+                    ),
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: const BoxDecoration(
+                        color: Color(0xff4CAF50),
+                      ),
+                      child: Container(
+                        height: 150,
+                        width: 150,
+                        alignment: Alignment.center,
+                        decoration:const  BoxDecoration(
+                        color: Color(0xff64FFDA),
+                        ),
                   ),
                 ),
-              ),
             ),
           ),
         ),
+      ),
+      ),
+      ),
       ),
     );
   }
